93b0a3068d52958b912ef8a6b09cf8454df546d5
gustavolara <gustavolara63719@gmail.com>
2023-12-14 15:43:18 -0300

    Header Finalizado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,26 @@
-import styles from "./Header.module.css";
+import * as C from "./styled.js";
+
 import linkedinLogo from "../../assets/logo-linkedin.svg"
 import githubLogo from "../../assets/logo-github.svg"
 import telegramLogo from "../../assets/logo-telegram.svg"
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>To-Do List</h1>
-        <ul className={styles.socialMedia}>
-          <li>
-            <a className={styles.link} href="https://linkedin.com" target="blank" rel="noopener noreferrer">
-              <img src={linkedinLogo} alt="" />
-            </a>
-          </li>
-          <li>
-            <a className={styles.link} href="https://github.com" target="blank" rel="noopener noreferrer">
-              <img src={githubLogo} alt="" />
-            </a>
-          </li>
-          <li>
-            <a className={styles.link} href="https://t.me" target="blank" rel="noopener noreferrer">
-              <img src={telegramLogo} alt="" />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </header>
+    <C.Header>
+      <C.Container>
+        <C.Title>To-Do List</C.Title>
+        <C.SocialMedia>
+          <C.List>
+            <C.Link href="https://linkedin.com" target="blank" rel="noopener noreferrer"><img src={linkedinLogo} alt="" /></C.Link>
+          </C.List>
+          <C.List>
+            <C.Link href="https://github.com" target="blank" rel="noopener noreferrer"><img src={githubLogo} alt="" /></C.Link>
+          </C.List>
+          <C.List>
+            <C.Link href="https://t.me" target="blank" rel="noopener noreferrer"><img src={telegramLogo} alt="" /></C.Link>
+          </C.List>
+        </C.SocialMedia>
+      </C.Container>
+    </C.Header>
   );
 };
 
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,5 +1,47 @@
 import styled from "styled-components";
 
-export const Header = styled.header `
+export const Header = styled.header`
     border-bottom: 1px solid #151a37;
+`
+
+export const Container = styled.div`
+    max-width: 1248px;
+    padding: 20px 16px;
+    margin: 0 auto;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 16px;
+`
+export const Title = styled.h1`
+    font-weight: 400;
+    line-height: 1;
+    font-weight: 400;
+`
+
+export const SocialMedia = styled.ul`
+    display: flex;
+    gap: 24px;
+`
+
+export const List = styled.li`
+
+`
+
+
+export const Link = styled.a`
+    width: 40px;
+    height: 40px;
+    background: linear-gradient(180deg, #151a37 0%, rgba(21, 26, 55, 0)100%);
+    border: 1px solid #24293f;
+    border-radius: 12px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.4s;
+
+    &:hover{
+        scale: 1.1;
+    }
 `
